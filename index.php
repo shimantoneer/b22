8f2eff4c6e8e1b8a14c1f54b05d65b955079081e
shimantoneer <poddernakla@gmail.com>
2018-09-11 21:40:31 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.amazon.com/gp/product/B07DCPLBRJ/ref=s9_acsd_top_hd_bw_b2MFa_c_x_2_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-4&pf_rd_r=P2C4AMGB4CYG8TWZ24QB&pf_rd_t=101&pf_rd_p=47af43ee-6d17-5f9c-af81-6a242b3615d7&pf_rd_i=562190");
+header("Location: http://www.coquitlam.ca/city-hall/bylaws/frequently-requested/tree-management-bylaw.aspx");
 die();
 ?>
